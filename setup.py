39c50be7a059829ae9a8b89ee029562cc5f188ee
mvergerdelbove <mvergerdelbove@work4labs.com>
2015-04-10 12:01:29 -0700

    add classifiers

--- setup.py
+++ setup.py
@@ -10,5 +10,18 @@ setup(
     author_email='mvdb@work4labs.com',
     url='https://github.com/m-vdb/confluence-python',
     packages=['confluence'],
-    scripts=["bin/confluence-cli"]
+    scripts=["bin/confluence-cli"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
